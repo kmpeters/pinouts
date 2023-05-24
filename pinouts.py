ae5dd5a409e8bd459dd2090ce781cd6b39a70977
Kevin Peterson <kmpeters@anl.gov>
2023-05-24 12:40:17 -0500

    Improved the formatting

--- pinouts.py
+++ pinouts.py
@@ -17,6 +17,9 @@ def readJsonFile(filename):
 data1 = readJsonFile(file1)
 data2 = readJsonFile(file2)
 
+print("{:^8}     {:^8}".format(data1['device'], data2['device']))
+print("========     ========")
+
 for pinDict in data1['pinout']:
 	#print(pinDict['pin'], pinDict['name'])
 	
@@ -26,7 +29,7 @@ for pinDict in data1['pinout']:
 		#print(pinDict['pin'], pinDict['function'],'\t', otherPinDict['pin'], otherPinDict['function'])
 		
 		if pinDict['function'] == otherPinDict['function']:
-			print(pinDict['pin'], pinDict['function'],'\t', otherPinDict['pin'], otherPinDict['function'])
+			print("{:>4}  {:>2} --- {:<2}  {:<4} ".format(pinDict['function'], pinDict['pin'], otherPinDict['pin'], otherPinDict['function']))
 			break
 	#print()
 	
